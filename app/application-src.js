afac44e144d875b53c516eeca40d2400bc7a6b34
schnubb <schnubb+git@gmail.com>
2015-01-10 11:39:24 +0100

    Fixed path errors
    Added some exeption handling, and fixed path-problems.

--- app/application-src.js
+++ app/application-src.js
@@ -1,28 +1,34 @@
 var fs 			= require('fs')
 ,	path		= require('path')
-,	dir 		= __dirname + '/'
+,	dir 		= __dirname
 ,	filename 	= 'data.txt';
 
-parseDomains = function(data){
-	var trial = data.split(':');
+parseDomains = function(str){
+	var trial = str.split(':');
 	if(trial[1].length !== 4) return false;
 	else return true;
 }
 
-exports.init = function(cb) {
-	fs.readFile( path.join(dir, filename), function(e, content){
-		var domains = content.toString().split('\n'),
-		proxData=[];
-		if(domains[(domains.length-1)] === '') domains.pop();
-		if(parseDomains(domains[0])) {
-			domains.forEach(function(data) {
-				var d = data.split(':');
-				proxData[d[0]] = d[1];
-			});
-			console.log(domains.length + ' Hosts loaded, proceed with server init...');
-			cb(proxData);
+exports.init = function(next) {
+	fs.readFile( path.join(dir, '..', filename), function(e, cont){
+		if(e) throw new Error(e);
+
+		if (!cont) {
+			var domains = cont.toString().split('\n'),
+			proxData=[];
+			if(domains[(domains.length-1)] === '') domains.pop();
+			if(parseDomains(domains[0])) {
+				domains.forEach(function(str) {
+					var d = str.split(':');
+					proxData[d[0]] = d[1];
+				});
+				console.log(domains.length + ' Hosts loaded, proceed with server init...');
+				next(proxData);
+			}else{
+				console.log('ERROR: ' + domains)
+			}
 		}else{
-			console.log('ERROR: ' + domains)
+			throw new Error({Error: 'No valid domain configuration!'})
 		}
 	});
 }
--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var http = require("http")
 ,	vHostsLoader = require("./app/application-src")
 ,	proxy = require("http-proxy").createProxyServer({});
 
-vHostsLoader.init(function(conf){
+vHostsLoader.init(function(config){
 	var server = http.createServer(function(req,res){
 		proxy.web( req, res, {
-			target:"http://127.0.0.1:" + conf[req.headers.host] + "/",
+			target:"http://127.0.0.1:" + config[req.headers.host] + "/",
 			ws:true
 		}, function(e){
 			res.writeHead(404, { 'Content-Type' : 'text/plain' });
 			res.end('Requested Application not Found or Active.');
-			console.log(conf);
-			console.log(req.headers.host + ':' + conf[req.headers.host] + " :: " + e.code);
+			
+			console.log(req.headers.host + ':' + config[req.headers.host] + " :: " + e.code);
 		});
 	}).listen(80);
 });
