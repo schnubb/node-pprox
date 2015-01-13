3027bfddf161923b50ca54c9bd8f94f7103e0785
schnubb <schnubb+git@gmail.com>
2015-01-13 01:03:44 +0100

    data.txt parser hinzugefügt, fixed #2
    Added data.txt parse function. Removing `\r` with regex replace.

--- app/application-src.js
+++ app/application-src.js
@@ -3,30 +3,41 @@ var fs 			= require('fs')
 ,	dir 		= __dirname
 ,	filename 	= 'data.txt';
 
-parseDomains = function(str){
-	var trial = str.split(':');
-	if(trial[1].length !== 4) return false;
-	else return true;
+parseDomainPort = function(str){
+	if(str.length > 0) {
+		var trial = str.split(':');
+		if(trial[1].length !== 4) return false;
+		else return true;
+	} else return false;
+}
+
+parseDomainArray = function(domains) {
+	var ret = {},
+		l	= 0;
+
+	for(i in domains) {
+		if( domains[i] !== '' || domains[i].length !== 0 && parseDomainPort(domains[i]) ) {
+			d = domains[i].split(':');
+			ret[d[0]] = d[1];
+			l++;
+		}
+	}
+	ret.length = l;
+	console.log('returns:', ret.length, ret);
+	return ret;
 }
 
 exports.init = function(next) {
 	fs.readFile( path.join(dir, '..', filename), function(e, cont){
+
 		if(e) throw new Error(e);
 
-		if (!cont) {
-			var domains = cont.toString().split('\n'),
-			proxData=[];
-			if(domains[(domains.length-1)] === '') domains.pop();
-			if(parseDomains(domains[0])) {
-				domains.forEach(function(str) {
-					var d = str.split(':');
-					proxData[d[0]] = d[1];
-				});
-				console.log(domains.length + ' Hosts loaded, proceed with server init...');
-				next(proxData);
-			}else{
-				console.log('ERROR: ' + domains)
-			}
+		if (cont.toString().length > 0) {
+			var domains = parseDomainArray( cont.toString().replace(/(\r)$/gm,'').split('\n') );
+			console.log(domains.length + ' Hosts loaded, proceed with server init...');
+
+			next(domains);
+
 		}else{
 			throw new Error({Error: 'No valid domain configuration!'})
 		}
