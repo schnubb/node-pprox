3b7e8d18dcd261c7adad8210c6dffa4c09640230
schnubb <schnubb+git@gmail.com>
2015-01-14 01:23:35 +0100

    added data.txt parsing see #2
    Removing LineBreaks/New Lines but still some problems with BOM on none
    utf8 data files.
    Need some database in the background... -.-

--- app/application-src.js
+++ app/application-src.js
@@ -1,40 +1,33 @@
 var fs 			= require('fs')
-,	path		= require('path')
-,	dir 		= __dirname
-,	filename 	= 'data.txt';
+,	_			= require('underscore')
+;
 
-parseDomainPort = function(str){
-	if(str.length > 0) {
-		var trial = str.split(':');
-		if(trial[1].length !== 4) return false;
-		else return true;
-	} else return false;
+_log = function(stuff) {console.log(stuff)};
+
+parseDomainArray = function(doms) {
+	// cleanup
+	var str = doms.replace(/(\r\n|\n|\r)/gm,';;');
+	doms = str.split(';;');
+	var retArray=[];
+	doms = _.without(doms, '', ' ');
+	// cleanup done!
+	doms.forEach(function(data) {
+		d = data.split(':');
+		retArray[d[0]] = d[1];
+	});
+	// Why?!
+	retArray.shift();
+	// get it
+	return retArray;
 }
 
-parseDomainArray = function(domains) {
-	var ret = {},
-		l	= 0;
-
-	for(i in domains) {
-		if( domains[i] !== '' || domains[i].length !== 0 && parseDomainPort(domains[i]) ) {
-			d = domains[i].split(':');
-			ret[d[0]] = d[1];
-			l++;
-		}
-	}
-	ret.length = l;
-	console.log('returns:', ret.length, ret);
-	return ret;
-}
-
-exports.init = function(next) {
-	fs.readFile( path.join(dir, '..', filename), function(e, cont){
-
+exports.init = function(dirfile, next) {
+	fs.readFile(dirfile, function(e, cont){
 		if(e) throw new Error(e);
-
+		var domains = [];
 		if (cont.toString().length > 0) {
-			var domains = parseDomainArray( cont.toString().replace(/(\r)$/gm,'').split('\n') );
-			console.log(domains.length + ' Hosts loaded, proceed with server init...');
+			domains = parseDomainArray(cont.toString());
+			_log(_.size(domains) + domains.length + ' Hosts loaded, proceed with server init...');
 
 			next(domains);
 
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var http = require("http")
-,	vHostsLoader = require("./app/application-src")
-,	proxy = require("http-proxy").createProxyServer({});
+var http 			= require("http")
+,	path			= require('path')
+,	vHostsLoader 	= require("./app/application-src")
+,	proxy 			= require("http-proxy").createProxyServer({})
+,	dir 			= __dirname
+,	filename 		= 'data.txt'
+;
 
-vHostsLoader.init(function(config){
+startProx = function(config) {
 	var server = http.createServer(function(req,res){
 		proxy.web( req, res, {
 			target:"http://127.0.0.1:" + config[req.headers.host] + "/",
@@ -10,8 +14,17 @@ vHostsLoader.init(function(config){
 		}, function(e){
 			res.writeHead(404, { 'Content-Type' : 'text/plain' });
 			res.end('Requested Application not Found or Active.');
-			
+			console.log(config, config[req.headers.host], req.headers.host);
 			console.log(req.headers.host + ':' + config[req.headers.host] + " :: " + e.code);
 		});
 	}).listen(80);
-});
+
+	server.on('error', function(e) {
+		if (e.code == 'EADDRINUSE') {
+			console.log('Address in use... quitting');
+			server.close();
+		}
+	})
+}
+
+vHostsLoader.init(path.join(dir, filename), startProx );
