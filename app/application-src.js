7df511302a9e94fc9b495b9332a54ac412c3b0ce
schnubb <schnubb+git@gmail.com>
2017-10-23 23:26:34 +0200

    fixes spaces rewrite some functions updates http-proxy dependancy

--- app/application-src.js
+++ app/application-src.js
@@ -1,38 +1,39 @@
-var fs 			= require('fs')
-,_			= require('underscore')
+const fs = require('fs')
+,_ = require('underscore')
 ;
 
-_log = function(stuff) {console.log(stuff)};
+_log = function(stuff) { console.log(stuff) };
 
 parseDomainArray = function(doms) {
-	// cleanup
-	var str = doms.replace(/(\r\n|\n|\r)/gm,';;');
-	doms = str.split(';;');
-	var retArray=[];
-	doms = _.without(doms, '', ' ');
-	// cleanup done!
-	doms.forEach(function(data) {
-		d = data.split(':');
-		retArray[d[0]] = d[1];
-	});
-	// Why?!
-	retArray.shift();
-	// get it
-	return retArray;
-}
+  // cleanup
+  const str = doms.replace(/(\r\n|\n|\r)/gm,';;');
+  const retArray = [];
+  doms = str.split(';;');
+  doms = _.without(doms, '', ' ');
+  // cleanup done!
+
+  doms.forEach((data) => {
+    d = data.split(':');
+    retArray[d[0]] = d[1];
+  });
+  retArray.length = doms.length;
+  // Why?!
+  retArray.shift();
+  // get it
+  return retArray;
+};
 
 exports.init = function(dirfile, next) {
-	fs.readFile(dirfile, function(e, cont){
-		if(e) throw new Error(e);
-		var domains = [];
-		if (cont.toString().length > 0) {
-			domains = parseDomainArray(cont.toString());
-			_log(_.size(domains) + domains.length + ' Hosts loaded, proceed with server init...');
+  fs.readFile(dirfile, function(e, cont) {
+      if(e) throw new Error(e);
+      let domains = [];
+      if (cont.toString().length > 0) {
+        domains = parseDomainArray(cont.toString());
+        _log(_.size(domains) + domains.length + ' Hosts loaded, proceed with server init...');
 
-			next(domains);
-
-		}else{
-			throw new Error({Error: 'No valid domain configuration!'})
-		}
-	});
-}
+        next(domains);
+      } else {
+        throw new Error({msg: 'No valid domain configuration!'})
+      }
+  });
+};
--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var 	http 			= require("http")
-,	path			= require('path')
-,	vHostsLoader 		= require("./app/application-src")
-,	proxy 			= require("http-proxy").createProxyServer({})
-,	dir 			= __dirname
-,	filename 		= 'data.txt'
+var http        = require('http')
+, path          = require('path')
+, vHostsLoader  = require('./app/application-src')
+, proxy         = require('http-proxy').createProxyServer({})
+, dir           = __dirname
+, filename      = 'data.txt'
 ;
 
 startProx = function(config) {
-	var server = http.createServer(function(req,res){
-		proxy.web( req, res, {
-			target:"http://127.0.0.1:" + config[req.headers.host] + "/",
-			ws:true
-		}, function(e){
-			res.writeHead(404, { 'Content-Type' : 'text/plain' });
-			res.end('Requested Application not Found or Active.');
-			console.error(req.headers.host + ':' + config[req.headers.host] + " :: " + e.code);
-		});
-	}).listen(80);
-	
-	server.on('error', function(e) {
-		if (e.code == 'EADDRINUSE') {
-			console.log('Address in use... quitting');
-			server.close();
-		} else {
-			console.log(e);
-		}
-	})
-}
+  var server = http.createServer( function(req, res) {
+    proxy.web( req, res, {
+    target: 'http://127.0.0.1:' + config[req.headers.host] + '/',
+    ws:true
+  }, function(e) {
+    res.writeHead(404, { 'Content-Type' : 'text/plain' });
+    res.end('Requested Application not Found or Active.');
+    console.error(req.headers.host + ':' + config[req.headers.host] + ' :: ' + e.code);
+    });
+  }).listen(80);
+
+  server.on('error', function(e) {
+    if (e.code == 'EADDRINUSE') {
+      console.log('Address in use... quitting');
+      server.close();
+    } else {
+      console.log(e);
+    }
+  })
+};
 
 vHostsLoader.init(path.join(dir, filename), startProx );
